b0058003f58e99f0eab08a61eee4de830bec24f3
Rodrigo Notini <rodrigo.ferr.not@gmail.com>
2025-10-22 01:15:40 -0300

    Testando commit no Visual Studio

--- captura/captura.cpp
+++ captura/captura.cpp
@@ -4,6 +4,7 @@
 
 int main() {
     atr::log_info("captura", "Skeleton pronto. Em fases futuras: consumir buffer1 e demux 11/44.");
+	atr::log_error("captura", "Erro simulado para teste de log_error.");
     std::cout << "OK captura.\n";
     return 0;
 }
